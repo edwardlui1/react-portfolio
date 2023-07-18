723d4b5c069c73d11b6f207dcfd6e949bddc6b9c
Edward Lui <luiedward0903@gmail.com>
2023-07-18 12:26:21 +1000

    header

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
-import './header.css'
+import React from 'react';
+import './header.css';
+import Image from '../../assets/images/avatar.png';
+
 
 const Header = () => {
   return (
     <header id="header">
       <div className="container header_container">
-        <h1>Hi, I'm Edward Lui</h1>
-        <h2>A full-stack developer</h2>
+        <div className="header_content">
+          <div className="header_text">
+            <h1>Hi, I'm Edward Lui</h1>
+            <h2>Full-stack developer</h2>
+            <button className="know_more_button" onClick={() => window.location.href = '#about'}>
+              Want to know more? Click me!
+            </button>
+          </div>
+          <div className="header_image">
+            <img src={Image} alt="Profile" />
+          </div>
+        </div>
       </div>
     </header>
-  )
-}
+  );
+};
+
 
 export default Header
